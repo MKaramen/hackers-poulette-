0257f8a88a3efce886ecc62dd8f6aedd1bbd1874
MKaramen <matis.kara@gmail.com>
2019-10-14 21:36:02 +0200

    debug done

--- inputUser.php
+++ inputUser.php
@@ -16,19 +16,23 @@
 
     function regExString($topic) {
         if(isset($_POST["submit"])) {
-             $respect = preg_match("/^[a-zA-Z ,.'-]+$/i", $_POST[$topic]);
+             $respect;
 
              if($topic == "email") {
                  $respect = preg_match( "/([a-zA-Z0-9.-]+@[a-zA-Z0-9.-]+).[a-zA-Z_-]{2,4}$/", $_POST[$topic]);
              }
 
+             else {
+               $respect = preg_match("/^[a-zA-Z ,.'-]+$/i", $_POST[$topic]);
+             }
+
              if($respect == 0 AND !empty($_POST[$topic])) {
-                if ($topic == 'textArea' ) {
-                    echo "<p class='text-center error invalid-feedback'>Invalid format</p>";
+                if ($topic == "textArea"     ) {
+                    echo "<p class='text-center error'>Invalid format</p>";
                     $_POST[$topic] = '';
                 }
                 else {
-                    echo "<p class='offset-md-3 col-md-6 error invalid-feedback'>Invalid format</p>";
+                    echo "<p class='offset-md-3 col-md-6 error'>Invalid format</p>";
                     $_POST[$topic] = '';
                 }
             }
@@ -36,11 +40,10 @@
     }
 
     function allGood() {
-        if(!empty($_POST['name']) AND !empty($_POST['firstName']) AND !empty($_POST['gender']) AND !empty($_POST['country']) AND !empty($_POST['email']) AND !empty($_POST['textArea'] AND isset($_POST['submit']))) {
+        if(!empty($_POST['name']) AND !empty($_POST['firstName']) AND !empty($_POST['gender']) AND !empty($_POST['country']) AND !empty($_POST['email']) AND !empty($_POST['textArea']) AND isset($_POST['submit'])) {
             $to      = $_POST['email'];
             $subject = "We received your message";
             $message = 'We received your message \n'.$_POST['textArea']."We'll try to respond as soon as possible.";
-
             mail($to, $subject, $message);
         }
     }
